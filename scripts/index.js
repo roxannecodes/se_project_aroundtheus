5e97b3ea60ad66848bc2842fa37d33bf4c3eb611
Roxanne <roxanne.shoar@gmail.com>
2024-03-10 21:36:56 -0400

    Fix form submit feature

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,10 @@
-//TODO: store initial cards info in an array of objects (stage 1 )
 
-let initialCards = [
+
+//TODO: store initial cards' data into an array of objects 
+const initialCards = [
   {
     name: "Yosemite Valley",
-    link: "https: //practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
   },
   {
     name: "Lake Louise",
@@ -27,46 +28,58 @@ let initialCards = [
   },
 ];
 
-//TODO: (stage 2 & 4)
-
-for (let i = 0; i < initialCards.length; i++) {
-  console.log(initialCards[i].name);
-  console.log(initialCards[i].link, "\n");
-}
-
-// TODO: Handle modal open and close events (stage 3)
+// TODO: Handle modal open and close events
 
 let modal = document.querySelector(".modal");
 let profileName = document.querySelector(".profile__name");
 let profileDescription = document.querySelector(".profile__description");
-let inputName = modal.querySelector(".modal__name");
-let inputDescription = modal.querySelector(".modal__description");
+let inputName = modal.querySelector("#profile-name");
+let inputDescription = modal.querySelector("#profile-description");
+
+
 
 let modalOpenButton = document.querySelector(".profile__edit-button");
 modalOpenButton.addEventListener("click", () => {
+  inputName.value = profileName.textContent;
+  inputDescription.value = profileDescription.textContent;
   modal.classList.add("modal_opened");
-}
-);
+});
 
 let modalCloseButton = modal.querySelector(".modal__close-button");
 modalCloseButton.addEventListener("click", () => {
   modal.classList.remove("modal_opened");
-}
-);
+});
 
-//TODO: Handle submit button events (stage 4-final) 
+//TODO: Handle submit button events
 
-let modalSubmitButton = modal.querySelector(".modal__save-button");
-modalSubmitButton.addEventListener("click", (evt) => {
+let modalForm = document.querySelector(".modal__form");
+modalForm.addEventListener("submit", (evt) => {
   evt.preventDefault();
   profileName.textContent = inputName.value;
   profileDescription.textContent = inputDescription.value;
   modal.classList.remove("modal_opened");
+});
+
+//TODO: Clone card template and fill element with user data
+
+let cardsContainer = document.querySelector(".cards__list");
+
+function getCardElement(data) {
+  let cardTemplate = document.querySelector("#card-template")
+    .content.querySelector(".card");
+  let cardElement = cardTemplate.cloneNode(true);
+  let cardImage = cardElement.querySelector(".card__image");
+  let cardTitle = cardElement.querySelector(".card__title");
+
+  cardImage.src = data.link;
+  cardImage.alt = data.name;
+  cardTitle.textContent = data.name;
+  return cardElement;
 }
-);
 
-  
-    
-  
-  
+//TODO: Iterate over cards to render grid
 
+for (let i = 0; i < initialCards.length; i++) {
+  let card = getCardElement(initialCards[i]);
+  cardsContainer.append(card);
+}
