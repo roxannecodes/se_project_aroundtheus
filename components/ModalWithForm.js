bd3225f1ee53ee2b3e7613e117c66865971c2a41
Roxanne <roxanne.shoar@gmail.com>
2025-10-29 18:15:11 -0400

    Code ModalWithForm sub-class  (complete)

--- components/ModalWithForm.js
+++ components/ModalWithForm.js
@@ -1,4 +1,6 @@
-export default class ModalWithForm {
+import Modal from "./Modal.js";
+
+export default class ModalWithForm extends Modal {
   constructor(modalSelector, handleFormSubmit) {
     super(modalSelector);
     this._handleFormSubmit = handleFormSubmit;
