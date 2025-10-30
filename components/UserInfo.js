361a72e7ec54c1750558c45a26f504ffb24cdac0
Roxanne <roxanne.shoar@gmail.com>
2025-10-29 23:20:37 -0400

    Code UserInfo class

---
+++ components/UserInfo.js
@@ -0,0 +1,19 @@
+export default class UserInfo {
+  constructor({ nameSelector, titleSelector }) {
+    this._userName = document.querySelector(nameSelector);
+    this._userTitle = document.querySelector(titleSelector);
+  }
+
+  getUserInfo() {
+    const currentUserInfo = {
+      name: this._userName.textContent,
+      title: this._userTitle.textContent,
+    };
+    return currentUserInfo;
+  }
+
+  setUserInfo(data) {
+    this._userName.textContent = data.name;
+    this._userTitle.textContent = data.title;
+  }
+}
