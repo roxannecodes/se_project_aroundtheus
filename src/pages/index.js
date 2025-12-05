6679fb5a59a5af58b711f0e6188c65b2fd6cb364
Roxanne <roxanne.shoar@gmail.com>
2025-12-04 22:44:39 -0500

    Chore: minor reformat of code & edit pseudo-code comments throughout for clarfication and conssistency; fix html syntax error

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,38 +10,6 @@ import ModalWithForm from "../components/ModalWithForm.js";
 import UserInfo from "../components/UserInfo.js";
 import Api from "../components/Api.js";
 
-// TODO: Fetch user info and initial cards from API and render them
-
-let cardList; // to hold Section instance for cards
-
-let cardToDelete;
-
-function renderPage() {
-  return Promise.all([api.getUserInfo(), api.getInitialCards()])
-    .then(([userInfoObj, initialCardsArr]) => {
-      //render user info
-      userInfo.setUserInfo({
-        name: userInfoObj.name,
-        description: userInfoObj.about,
-      });
-      //Instantiate & initialize Section class for rendering initial cards
-      cardList = new Section(
-        {
-          items: initialCardsArr.reverse(),
-          renderer: (item) => {
-            const cardElement = createCard(item);
-            cardList.addItem(cardElement);
-          },
-        },
-        ".cards__list"
-      );
-      cardList.renderItems();
-    })
-    .catch((error) => {
-      console.error("Problem rendering cards.", error);
-    });
-}
-
 // Instantiate class for API interactions
 const api = new Api({
   baseUrl: "https://around-api.en.tripleten-services.com/v1",
@@ -51,35 +19,76 @@ const api = new Api({
   },
 });
 
-//Instantiate class for getting/setting user info
+//Instantiate class for getting & setting user info
 const userInfo = new UserInfo({
   nameSelector: ".profile__name",
   titleSelector: ".profile__description",
 });
 
-// Initial render of user profile & cards on page load
+// TODO: Display the user's profile info and card posts when page loads
+
+let cardList; // to hold Section instance for cards
+
+function renderPage() {
+  // Call api to fetch user's current info and cards data
+  return (
+    Promise.all([api.getUserInfo(), api.getInitialCards()])
+      // and then, if successfully reached:
+      .then(([userInfoObj, initialCardsArr]) => {
+        // Render user info
+        userInfo.setUserInfo({
+          name: userInfoObj.name,
+          description: userInfoObj.about,
+        });
+        // Instantiate Section class for creating the cards and adding them to the DOM
+        cardList = new Section(
+          {
+            items: initialCardsArr.reverse(),
+            renderer: (item) => {
+              const cardElement = createCard(item);
+              cardList.addItem(cardElement);
+            },
+          },
+          ".cards__list"
+        );
+        // and ititialize it
+        cardList.renderItems();
+      })
+      .catch((error) => {
+        console.error("Error rendering cards:", error);
+      })
+  );
+}
+
+// Inital call to render current user info & cards list
 renderPage();
 
 //TODO: Handle opening & submitting [PROFILE MODAL]
 
-// Instantiate subclass & initalize
+// Instantiate edit profile popup subclass
 const profileModal = new ModalWithForm("#profile-modal", submitProfileForm);
+// and initialize it
 profileModal.setEventListeners();
 
-// Open edit profile modal
+// ** Open ** edit profile modal
 const profileEditButton = document.querySelector(".profile__edit-button");
 
 profileEditButton.addEventListener("click", () => {
   profileModal.open();
+  // Reset validation errors in case the user previously closed modal containing invalid input data
   profileFormValidation.resetValidation();
+  // Get users current info from DOM
   const currentInfo = userInfo.getUserInfo();
+  // Set the current values in form inputs
   profileModal.setInputValues(currentInfo);
 });
 
-// Submit profile modal form
+// ** Submit ** profile popup form to edit user's name & about info
 function submitProfileForm(data) {
+  // Call api to access user's current profile data
   api
     .editUserInfo(data)
+    // and update it if successful
     .then((updatedUserInfo) => {
       userInfo.setUserInfo({
         name: updatedUserInfo.name,
@@ -93,13 +102,14 @@ function submitProfileForm(data) {
     });
 }
 
-//TODO: handle opening & submitting [ADD-CARD MODAL]
+//TODO: handle opening & submitting [CARD MODAL] to add new card
 
-// Instantiate subclass & initalize
+// Instantiate new card popup subclass
 const cardModal = new ModalWithForm("#card-modal", submitCardForm);
+// and initialize it
 cardModal.setEventListeners();
 
-// ** Open ** add-card modal
+// ** Open ** new card modal
 const addCardButton = document.querySelector(".profile__add-button");
 
 addCardButton.addEventListener("click", () => {
@@ -107,10 +117,12 @@ addCardButton.addEventListener("click", () => {
   cardFormValidation.resetValidation();
 });
 
-// ** Submit **  add-card modal form
+// ** Submit ** the card popup form to add to page
 function submitCardForm(data) {
+  //call api at the /cards endpoint to store card
   api
     .addNewCard(data)
+    // and then create new card and add it the DOM if POST call is successful
     .then((newCardData) => {
       const cardElement = createCard(newCardData);
       cardList.addItem(cardElement);
@@ -122,26 +134,30 @@ function submitCardForm(data) {
     });
 }
 
-//TODO:  Handle opening [PREVIEW MODAL] on card image click
+//TODO:  Handle opening [PREVIEW MODAL] to view a larger card image
 
-// Instantiate subclass & initalize
+// Instantiate the image popup subclass
 const previewModal = new ModalWithImage("#preview-modal");
+// and initialize it
 previewModal.setEventListeners();
 
+// ** Open ** image popup on card image click
 function openPreviewModal(data) {
   previewModal.open(data);
 }
 
-//TODO: Handle opening & submitting [CONFIRMATION MODAL]
+//TODO: Handle opening & submitting [CONFIRMATION MODAL] to delete card
 
-//instantiate subclass & initalize
+// Instantiate confirmation popup subclass
 const confirmationModal = new ModalWithForm(
   "#confirmation-modal",
   submitConfirmationForm
 );
+// and initialize it
 confirmationModal.setEventListeners();
 
-//Open confirmation modal on delete button click in Card class
+// ** Open ** confirmation modal on delete button click
+let cardToDelete;
 
 function openConfirmationModal(card) {
   cardToDelete = card;
@@ -149,20 +165,20 @@ function openConfirmationModal(card) {
   confirmationModal.setInputValues({ cardId: card.cardId });
 }
 
-// Submit confirmation modal form to delete card
+// ** Submit ** confirmation modal form to delete card
 
 function submitConfirmationForm(data) {
-  //call API to delete the card from the server
-  // and then remove it from the DOM if the API call is successful.
+  //call API to delete the card
   api
     .deleteCard(data.cardId)
+    // and then remove it from the DOM if the API call is successful.
     .then(() => {
       cardToDelete.removeCard();
       cardToDelete = null;
       confirmationModal.close();
     })
     .catch((error) => {
-      console.error("Failed to delete card!");
+      console.error(`Failed to delete card:${error}`);
     });
 }
 
@@ -178,9 +194,10 @@ function createCard(data) {
   return card.generateCard();
 }
 
-//TODO: instantiating & enabling [form validator] for both modals
+//TODO: [Form validation] for the editing profile and adding a new card popup forms
 
-export const config = {
+// Set options object for configuration
+const config = {
   formSelector: ".modal__form",
   inputSelector: ".modal__input",
   submitButtonSelector: ".modal__save-button",
@@ -188,11 +205,14 @@ export const config = {
   inputErrorClass: "modal__input_type_error",
   errorClass: "modal__input-error_active",
 };
+
+// Search for the forms
 const profileModalForm = document.forms["profile-form"];
 const cardModalForm = document.forms["card-form"];
 
+//Instantiate validation for both forms
 const profileFormValidation = new FormValidator(config, profileModalForm);
-profileFormValidation.enableValidation();
-
 const cardFormValidation = new FormValidator(config, cardModalForm);
+// and enable them when the page loads
+profileFormValidation.enableValidation();
 cardFormValidation.enableValidation();
